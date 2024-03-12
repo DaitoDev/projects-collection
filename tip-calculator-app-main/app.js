74ce77ff65ab1fc3d0a673bb5c70b216b2f65b23
Daito Dave <82598098+DaitoDev@users.noreply.github.com>
2024-03-12 19:04:56 -0400

    Add tip calculator project
    2nd frontend mentor project added

---
+++ tip-calculator-app-main/app.js
@@ -0,0 +1,34 @@
+const bill = document.getElementById("bill").value;
+const people = document.getElementById("people").value;
+
+const btn = document.querySelectorAll(".tip-btn");
+
+const tipRes = document.querySelector(".tip-res");
+const tipTotal = document.querySelector(".tip-total");
+const billTotal = document.querySelector(".bill-total");
+
+function findTip(percent, ebill) {
+  let tip = (percent / 100) * ebill;
+  return tip;
+}
+
+function displayResults(tip, total, final) {
+  tipRes.textContent = `${tip}`;
+  tipTotal.textContent = `${total}`;
+  billTotal.textContent = `${final}`;
+}
+
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const bill = document.getElementById("bill").value;
+    const people = document.getElementById("people").value;
+
+    let res = findTip(button.value, bill);
+
+    let amt = res / people;
+
+    let finalBill = Number(res) + Number(bill);
+
+    displayResults(amt.toFixed(2), res.toFixed(2), finalBill.toFixed(2));
+  });
+});
